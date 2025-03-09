5de3072bc04e16a59df3b06b58774c86c9fe683d
yiqingxiao <xiaoyiqing90@gmail.com>
2025-03-08 19:36:52 -0500

    Creation of the base class for the card pick reaction game.

---
+++ SFML_games/cardReact.h
@@ -0,0 +1,36 @@
+#ifndef CARDREACT_H
+#define CARDREACT_H
+
+
+
+#include <cmath>
+#include <iostream>
+#include <magic_enum.hpp>
+#include <string>
+
+
+using namespace std;
+
+
+/*
+This is a reaction game through picking cards.
+One card is faced down at the beginning of the game.
+All possible cards this hidden card can be are displayed by its side.
+After a countdown, the faced down card is revealed, and the player must select
+the matching card to the revealed card as fast as possible.
+*/
+class cardReact{
+
+
+
+};
+
+
+
+
+
+
+
+
+
+#endif  // CARDREACT_H
