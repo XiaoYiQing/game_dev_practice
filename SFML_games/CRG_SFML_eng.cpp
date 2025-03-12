4c2bb87e8c72d914daff97e0c6b16e05edfb1c5f
yiqingxiao <xiaoyiqing90@gmail.com>
2025-03-12 15:37:08 -0400

    Constructor for teh SFML version of CRG

--- SFML_games/CRG_SFML_eng.cpp
+++ SFML_games/CRG_SFML_eng.cpp
@@ -2,4 +2,48 @@
 
 
 
+using namespace gameEngine;
+
+CRG_SFML_eng::CRG_SFML_eng( int possCardCnt = 8, long long cntDownT = 3000 ) : 
+    cardReact( possCardCnt, cntDownT )
+{
+
+    // Determine the number of rows and columns of cards depending on how many cards there
+    // are in total.
+    float tmp = std::sqrt( (float) possCardCnt );
+    rowColCnt.x = std::ceil( ( (float) possCardCnt )/tmp );
+    rowColCnt.y = std::ceil( tmp );
+    
+
+    // Define the default locations and sizes of the buttons.
+    float x_start = 100;        float y_start = 100;
+    float butWidth = 50;       float butHeight = 50;
+    float butSep = 5;
+
+    // Card index variable.
+    int z = 0;
+    for( int i = 0; ( i < rowColCnt.x && z < possCardCnt ); i++ ){
+        for( int j = 0; ( j < rowColCnt.y && z < possCardCnt ); j++ ){
+
+            possCardTex_vect.push_back( shared_ptr<sf::Texture>( new sf::Texture() ) );
+
+            shared_ptr<SFML_button_XYQ> buttonX = 
+                    shared_ptr<SFML_button_XYQ>( new SFML_button_XYQ() );
+
+            buttonX->setPos( x_start + ( butWidth + butSep )*j, 
+                y_start + ( butHeight + butSep )*i );
+            buttonX->setWidth( butWidth );      
+            buttonX->setHeight( butHeight );  
+            buttonX->setTxtStr( "" + z );
+            buttonX->disableSprite();
+
+            possCard_vect.push_back( buttonX );
+
+            z++;
+
+        }
+
+    }
+
+}
 
--- SFML_games/CRG_SFML_eng.h
+++ SFML_games/CRG_SFML_eng.h
@@ -6,6 +6,7 @@
 
 #include <SFML/Graphics.hpp>
 
+#include "SFML_button_XYQ.h"
 
 using namespace std;
 
@@ -20,9 +21,58 @@ class CRG_SFML_eng : cardReact{
 public:
 
 
+// ====================================================================== >>>>>
+//      Constructor
+// ====================================================================== >>>>>
 
+    CRG_SFML_eng( int possCardCnt = 8, long long cntDownT = 3000 );
+
+// ====================================================================== <<<<<
+
+
+// ====================================================================== >>>>>
+//      Access Functions (public)
+// ====================================================================== >>>>>
+
+// ====================================================================== <<<<<
+
+
+// ====================================================================== >>>>>
+//      Gameplay Functions
+// ====================================================================== >>>>>
+
+
+// ====================================================================== <<<<<
 
 protected:
+
+// ====================================================================== >>>>>
+//      Access Functions (protected)
+// ====================================================================== >>>>>
+
+// ====================================================================== <<<<<
+
+    /*
+    Vector holding the number of rows (x) and columns (y).
+    */
+    sf::Vector2i rowColCnt;
+
+    /*
+    The main card which is hidden at first when the game starts and is then
+    revealed when the count down is finished.
+    */
+    shared_ptr<SFML_button_XYQ> mainCard;
+
+    /*
+    The vector of buttons representing the possible cards the hidden card can 
+    share the same sprite with.
+    */
+    vector<shared_ptr<SFML_button_XYQ>> possCard_vect;
+
+    /*
+    Textures of the possible cards.
+    */
+    vector<shared_ptr<sf::Texture>> possCardTex_vect;
 
 
 };
