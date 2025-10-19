0edfbc0d49368dcda39e0bdf221e83017f1df18a
yiqingxiao <xiaoyiqing90@gmail.com>
2025-10-18 20:07:05 -0400

    chess: CHS_STATE enum correction.

--- SFML_games/chess.cpp
+++ SFML_games/chess.cpp
@@ -46,13 +46,13 @@ chess::CHS_PIECE_COLOR chess::get_CHS_PIECE_COLOR_AtIdx( int idx ){
 //      Class Enum "CHS_STATE" Help Functions
 // ====================================================================== >>>>>
 
-string checkers::get_CHS_STATE_Str( CHS_STATE tar_CHS_STATE ){
+string chess::get_CHS_STATE_Str( CHS_STATE tar_CHS_STATE ){
     return string( magic_enum::enum_name( tar_CHS_STATE ) );
 }
 
-checkers::CHS_STATE checkers::get_CHS_STATE_AtIdx( int idx ){
-    if( idx >= 0 && idx < checkers::CHS_STATE_Count ){
-        return static_cast<checkers::CHS_STATE>(idx);
+chess::CHS_STATE chess::get_CHS_STATE_AtIdx( int idx ){
+    if( idx >= 0 && idx < chess::CHS_STATE_Count ){
+        return static_cast<chess::CHS_STATE>(idx);
     }else{
         throw invalid_argument( "Invalid int index for accessing enum \"CHS_STATE\"." );
     }
